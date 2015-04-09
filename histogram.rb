6a211cb10a61fef753e6a19fa8a715387d6ed775
syberpunkq <syberpunkq@gmail.com>
2015-04-09 10:33:34 +0000

    stock picker added

--- histogram.rb
+++
@@ -1,9 +0,0 @@
-#program takes string and builds a hash where keys are unique words from this string and values are numbers of times that word occurs
-
-input_string = "people as people and you like an alien on the camel on the people and people and check check and people alien"
-#input_string = gets.chomp
-
-output_hash = Hash.new(0)
-text_array = input_string.split(" ")
-text_array.each {|item| output_hash[item]+=1}
-puts output_hash.inspect
